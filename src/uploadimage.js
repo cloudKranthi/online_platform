b8c9d7eacd05fef36f016abbfb6c6ce58f85d3f0
cloudkranthi <shivakranthi0006@gmail.com>
2025-12-07 19:00:50 +0530

    this is fourth commit

--- src/uploadimage.js
+++ src/uploadimage.js
@@ -1,13 +1,16 @@
 const cloudinary = require('./utils/cloudinary');
 async function uploadImage(localFilePath){
     try{
-     const results = await cloudinary.uploader.upload(localFilePath)
-    console.log("IMAGE URI:",results.secure_url);
+     const results = await cloudinary.uploader.upload(localFilePath,{
+        resource_type:"auto"
+     })
+    console.log(" URI:",results.secure_url);
     return results.secure_url;
 
 
     }catch(error){
-    console.error("cloudinary upload error ",error)
+   fs.unlinkSync(localFilePath)
+   return null
     }
     
 }
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,5 @@
 const cloudinary = require('cloudinary');
+const fs = require('fs')
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
     api_key:process.env.CLOUDINARY_API_KEY,
